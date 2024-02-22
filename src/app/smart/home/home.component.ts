8048e46276ea90ed2d003d010e2fca897036c5d3
Vanessa Ortega <ortegavan@live.com>
2024-02-22 07:23:15 -0300

    feat: finalizada seção de skills

--- src/app/smart/home/home.component.ts
+++ src/app/smart/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+    constructor(private title: Title) {
+        this.title.setTitle(
+            'Vanessa Ortega | Frontend Developer | Angular | TypeScript | RxJS | UX | UI'
+        );
+    }
 }

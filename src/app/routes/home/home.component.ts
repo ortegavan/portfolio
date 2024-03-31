9040f09f88b304832d5e08c9554e0cb4b6b285d2
Vanessa Ortega <ortegavan@live.com>
2024-03-31 15:54:00 -0300

    feat: criada seção de habilidades

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
+import { HeroComponent } from '../../ui/hero/hero.component';
+import { MenuComponent } from '../../ui/menu/menu.component';
+import { FeaturesComponent } from '../../ui/features/features.component';
+import { ProjectsComponent } from '../../ui/projects/projects.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [ButtonModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
+    imports: [
+        HeroComponent,
+        MenuComponent,
+        FeaturesComponent,
+        ProjectsComponent,
+    ],
 })
 export class HomeComponent {}

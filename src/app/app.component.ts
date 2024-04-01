e90d64abf17c902e119293a80ab8e653e1adac0e
Vanessa Ortega <ortegavan@live.com>
2024-04-01 17:57:43 -0300

    feat: aplicada animação do aos ao menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import AOS from 'aos';
 import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
@@ -13,6 +14,9 @@ export class AppComponent {
     title = 'portfolio-v2';
 
     constructor(private primengConfig: PrimeNGConfig) {
+        AOS.init({
+            once: true,
+        });
         this.primengConfig.ripple = true;
     }
 }

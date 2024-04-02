944db5830dbda9971fdd3d9225b72ceb725ffb1d
Vanessa Ortega <ortegavan@live.com>
2024-04-02 02:49:34 -0300

    feat: implementado botão que leva para o topo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, ButtonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
@@ -14,5 +15,24 @@ export class AppComponent {
 
     constructor(private primengConfig: PrimeNGConfig) {
         this.primengConfig.ripple = true;
+    }
+
+    @HostListener('window:scroll', ['$event'])
+    onWindowScroll() {
+        const hide = 'hide';
+        let div = document.querySelector<HTMLDivElement>('#toUp');
+
+        if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+        ) {
+            div!.classList.remove(hide);
+        } else {
+            div!.classList.add(hide);
+        }
+    }
+
+    goToTop() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     }
 }
